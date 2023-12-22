d4c175eecb8e93f83856ea2229a002bf91ea3acf
kylo <103489352+kylo-dev@users.noreply.github.com>
2023-12-22 13:06:22 +0900

    [Silver III] Title: 계단 오르기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * 301
+stairs = [0] * 301
+
+for i in range(1, n+1):
+  stairs[i] = int(input())
+
+dp[1] = stairs[1]
+dp[2] = stairs[1] + stairs[2]
+dp[3] = max(stairs[2]+stairs[3], stairs[1]+stairs[3])
+
+for i in range(4, n+1):
+  dp[i] = max(stairs[i] + stairs[i-1] + dp[i-3], stairs[i] + dp[i-2])
+
+print(dp[n])
