89534e661a119dc7b803fd5c17d3d5dc9ee3aa37
kylo <103489352+kylo-dev@users.noreply.github.com>
2023-12-27 00:04:26 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -1,12 +1,16 @@
-N = int(input())
-li = list(map(int, input().split()))
+n = int(input())
+
+calls = list(map(int, input().split()))
+
 y = m = 0
-for n in li:
-    y += (n//30 + 1) * 10
-    m += (n//60 + 1) * 15
-if m == y:
-    print("Y M", m)
-elif m < y:
-    print("M", m)
+
+for i in calls:
+  y += (i//30 + 1) * 10
+  m += (i//60 + 1) * 15
+
+if y > m:
+  print("M", m)
+elif y < m:
+  print("Y", y)
 else:
-    print("Y", y)
+  print("Y M", y)
