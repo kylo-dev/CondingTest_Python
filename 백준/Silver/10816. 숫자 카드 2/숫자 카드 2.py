d7567569f0b60b6470ad4c6f804dd47c5d88c3c0
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-03-03 15:18:55 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1344 ms, Memory: 148996 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,31 +1,22 @@
 import sys
 input = sys.stdin.readline
+from collections import Counter
 
 n = int(input())
-nums = sorted(list(map(int, input().split())))
-
-m = int(input())
 cards = list(map(int, input().split()))
 
-dict = {}
+m = int(input())
+test = list(map(int, input().split()))
 
-for i in nums:
-  if i in dict:
-    dict[i] += 1
-  else:
-    dict[i] = 1
+cards = sorted(Counter(cards).items(), key = lambda x : x)
+cards = dict(cards)
 
-def binary(arr, target, start, end):
-  if (start > end):
-    return 0
-    
-  mid = (start+end)// 2
-  if arr[mid] == target:
-    return dict.get(target)
-  elif arr[mid] > target:
-    return binary(arr, target, start, mid-1)
+answer = []
+
+for i in test:
+  if i in cards:
+    answer.append(cards[i])
   else:
-    return binary(arr, target, mid+1, end)
-    
-for i in cards:
-  print(binary(nums, i, 0, n-1), end=" ")
+    answer.append(0)
+
+print(' '.join(map(str, answer)))
