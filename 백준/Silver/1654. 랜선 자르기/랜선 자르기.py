a35991066b7e1968ecefe84f71fe524cb9adb122
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-03-08 16:37:08 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -2,23 +2,20 @@ import sys
 input = sys.stdin.readline
 
 k, n = map(int, input().split())
+lans = [int(input()) for _ in range(k)]
 
-lan = [int(input()) for _ in range(k)]
-
-start, end = 1, max(lan)
+start, end = 1, max(lans)
 
 while start <= end:
-  mid = (start + end) // 2
+  mid = (start+end)//2
   lines = 0
 
-  for i in lan:
-    lines += i // mid
-    
+  for i in lans:
+    lines += (i//mid)
+
   if lines >= n:
-    # 조건 충족 시, 최대의 랜선 길이를 위해 start 값을 키우기
     start = mid + 1
   else:
-    # 조건 미충족 시, 조건 만족을 위해 랜선의 길이 줄이기. end 값 줄이기
     end = mid - 1
 
 print(end)
