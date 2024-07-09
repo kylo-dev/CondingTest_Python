1e2db5948fa63e63b3a52820aff7fea7f4702165
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-09 22:19:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,18 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 t = int(input())
 
-dp = [0] * 11
+for _ in range(t):
+  n = int(input())
+  dp = [0] * (n+1)
 
-for i in range(t):
-    n = int(input())
-    
-    for j in range(1, n+1):
-        if (j==1):
-            dp[j] = 1
-        elif(j==2):
-            dp[j] = 2
-        elif(j==3):
-            dp[j] = 4
-        else:
-            dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
-    print(dp[n])
-    
+  for i in range(1, n+1):
+    if i == 1:
+      dp[i] = 1
+    elif i == 2:
+      dp[i] = 2
+    elif i == 3:
+      dp[i] = 4
+    else:
+      dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+  print(dp[n])
