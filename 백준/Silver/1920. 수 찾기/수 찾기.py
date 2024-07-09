ec0a25a7788ccfb697cf63145ea456e9ad22bc62
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-09 23:23:52 +0900

    [Silver IV] Title: 수 찾기, Time: 396 ms, Memory: 47424 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(array, target, start, end):
-  while start <= end:
-    mid = (start + end) // 2
-    if array[mid] == target:
-      return 1
-    elif array[mid] > target:
-      end = mid - 1
-    else :
-      start = mid + 1
-  return 0
-
-
 n = int(input())
-array = list(map(int, input().split()))
-array.sort()
+
+arr = list(map(int, input().split()))
+arr.sort()
 
 m = int(input())
-test = list(map(int, input().split()))
-answer = []
+num = list(map(int, input().split()))
 
-for i in range(m):
-  answer.append(binary_search(array, test[i], 0, n-1))
+def binary_search(arr, start, end, n):
+  while start <= end:
+    mid = (start + end) // 2
+    if arr[mid] > n:
+      end = mid - 1
+    elif arr[mid] < n:
+      start = mid + 1
+    elif arr[mid] == n:
+      return True
 
-for i in answer:
-  print(i)
+for i in num:
+  if binary_search(arr, 0, len(arr)-1, i):
+    print(1)
+  else:
+    print(0)
