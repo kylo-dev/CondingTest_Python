b73ec0658846a116a653500fa235d918dad71451
kylo <103489352+kylo-dev@users.noreply.github.com>
2024-07-09 21:20:08 +0900

    [Silver I] Title: 그림, Time: 296 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,44 +1,34 @@
-import sys
-input = sys.stdin.readline
 from collections import deque
 
 n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-arr = [list(map(int, input().split())) for i in range(n)]
+cnt = 0
+max_sum = 0
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
 def bfs(x, y):
-  deq = deque([(x,y)])
-  cnt = 1
+  que = deque([(x, y)])
   arr[x][y] = 0
-
-  while deq:
-    a, b = deq.popleft()
-  
+  max_cnt = 1
+  while que:
+    x, y = que.popleft()
     for i in range(4):
-      nx = a + dx[i]
-      ny = b + dy[i]
-  
-      if -1 < nx < n and -1 < ny < m:
-        if arr[nx][ny] != 0:
-          arr[nx][ny] = 0
-          cnt += 1
-          deq.append((nx, ny))
-  return cnt
-
-num_list = []
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1:
+        que.append((nx, ny))
+        arr[nx][ny] = 0
+        max_cnt += 1
+  return max_cnt
 
 for i in range(n):
   for j in range(m):
     if arr[i][j] == 1:
-      cnt = bfs(i, j)
-      num_list.append(cnt)
+      cnt += 1
+      max_sum = max(max_sum, bfs(i, j))
 
-if len(num_list) == 0 :
-  print(len(num_list))
-  print(0)
-else:
-  print(len(num_list))
-  print(max(num_list))
+print(cnt)
+print(max_sum)
