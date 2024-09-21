14bf545f4e4ee9f22f67773dfa6f317600152992
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-09-21 15:54:11 +0900

    [Silver III] Title: 퇴사, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0 for _ in range(n+1)]
+
+for i in range(n):
+  for j in range(i + arr[i][0], n+1):
+    dp[j] = max(dp[j], dp[i] + arr[i][1])
+
+print(dp[-1])
