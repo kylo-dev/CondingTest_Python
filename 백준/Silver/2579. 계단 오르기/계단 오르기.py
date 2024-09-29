3474a509d259d41663d6d6c4d892eced2c7fa2c3
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-09-29 17:00:42 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,20 +1,16 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
 
-t = int(input())
+arr = [0] * 301
+for i in range(n):
+    arr[i] = int(input())
 
-dp = [0] * (t + 1)
-stair = [int(input()) for _ in range(t)]
+dp = [0] * 301
 
-for i in range(1, t+1):
+dp[0] = arr[0]
+dp[1] = arr[0] + arr[1]
+dp[2] = max(arr[2]+arr[1], arr[2]+arr[0])
 
-  if i == 1:
-    dp[i] = stair[i-1] # stair[0] : 1층
-  elif i == 2:
-    dp[i] = stair[i-2] + stair[i-1]
-  elif i == 3:
-    dp[i] = max((stair[i-2]+stair[i-1]), (stair[i-3] + stair[i-1]))
-  else:
-    dp[i] = max((stair[i-1] + stair[i-2] + dp[i-3]), (stair[i-1] + dp[i-2]))
+for i in range(3, n):
+  dp[i] = max(arr[i] + arr[i-1] + dp[i-3], arr[i] + dp[i-2])
 
-print(dp[t])
+print(dp[n-1])
