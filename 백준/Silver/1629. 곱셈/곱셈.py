b56faba4171c11032cc4138fba17d7e817f6d572
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2024-10-06 21:41:38 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,16 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 a, b, c = map(int, input().split())
 
 def multi(a, n):
   if n == 1:
     return a % c
+
+  tmp = multi(a, n//2)
+  if n % 2 == 0:
+    return (tmp * tmp) % c
   else:
-    tmp = multi(a, n//2)
-    if n % 2 == 0:
-      return (tmp * tmp) % c
-    else:
-      return (tmp * tmp * a) % c
+    return (tmp * tmp * a) % c
 
 print(multi(a, b))
