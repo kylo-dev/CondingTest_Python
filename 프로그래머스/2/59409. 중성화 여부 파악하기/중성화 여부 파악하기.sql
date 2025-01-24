a110be185773499801955f538c05fd7ab49b8fff
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-01-24 23:56:41 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,9 @@
+select animal_id, name, (
+        CASE 
+            WHEN sex_upon_intake LIKE '%Neutered%' THEN 'O'
+            WHEN sex_upon_intake LIKE '%Spayed%' THEN 'O'
+            ELSE 'X'
+        END
+    ) AS '중성화'
+from animal_ins
+order by animal_id
