334189131ece9c8754d6ca2a27d3b9ae77114858
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-02-04 00:24:14 +0900

    [Gold IV] Title: 뱀, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,14 +1,15 @@
+# 보드 크기
 n = int(input())
-k = int(input())
 data = [[0] * (n+1) for _ in range(n+1)]
-info = []
 
-# 사과 정보
+# 사과 개수
+k = int(input())
 for _ in range(k):
   a, b = map(int, input().split())
   data[a][b] = 1
 
 l = int(input())
+info = []
 for _ in range(l):
   x, c = input().split()
   info.append((int(x), c))
@@ -18,21 +19,21 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 def turn(direction, c):
-  if c == "L":
+  if c == 'L':
     direction = (direction - 1) % 4
   else:
     direction = (direction + 1) % 4
+
   return direction
 
 def simulate():
   x, y = 1, 1
-  data[x][y] = 2 # 뱀이 존재하는 위치 2로 표시
-
-  direction = 0 # 0: 동쪽
-  time = 0
+  direction = 0
   index = 0
-
+  time = 0
   q = [(x, y)]
+  data[x][y] = 2
+  
   while True:
     nx = x + dx[direction]
     ny = y + dy[direction]
@@ -40,8 +41,8 @@ def simulate():
     if 1 <= nx <= n and 1 <= ny <= n and data[nx][ny] != 2:
       if data[nx][ny] == 0:
         data[nx][ny] = 2
-        q.append((nx, ny))
         qx, qy = q.pop(0)
+        q.append((nx, ny))
         data[qx][qy] = 0
       elif data[nx][ny] == 1:
         data[nx][ny] = 2
@@ -52,7 +53,7 @@ def simulate():
 
     x, y = nx, ny
     time += 1
-    if index < l and time == info[index][0]:
+    if index < l and info[index][0] == time:
       direction = turn(direction, info[index][1])
       index += 1
   return time
