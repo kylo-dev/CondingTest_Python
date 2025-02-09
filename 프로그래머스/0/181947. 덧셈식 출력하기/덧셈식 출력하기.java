3dd96cac1916046175aeea99cc3817db76d8d323
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-02-09 17:45:07 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 169.42 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a + " + " + b + " = " + (a+b));
+        
+    }
+}
