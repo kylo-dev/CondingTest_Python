13a67586ead85c715cd2839738cf674ea20a82e2
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-02-24 20:57:13 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,14 +1,12 @@
-import sys
-input = sys.stdin.readline
 
 N, K = map(int, input().split())
 nums = list(map(int, input().split()))
 
-win_sum = sum(nums[:K])
-max_sum = win_sum
+max_sum = sum(nums[:K])
+win_sum = max_sum
 
 for i in range(K, N):
-  win_sum += nums[i] - nums[i-K]
+  win_sum = win_sum - nums[i-K] + nums[i]
   max_sum = max(max_sum, win_sum)
 
 print(max_sum)
