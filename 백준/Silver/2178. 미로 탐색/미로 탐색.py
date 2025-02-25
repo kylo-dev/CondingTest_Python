4a8d0918f7a48d9d97077c7d0b222ed43beff826
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-02-26 00:50:05 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,27 +2,34 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n, m = map(int, input().split())
+N, M = map(int, input().split())
+graph = []
+for _ in range(N):
+  graph.append(list(input().rstrip()))
 
-arr = [list(map(int, input().rstrip())) for _ in range(n)]
+for i in range(N):
+  for j in range(M):
+    graph[i][j] = int(graph[i][j])
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+visited = [[False] * M for _ in range(N)]
 
-def bfs(x, y):
-  que = deque([(x, y)])
+def bfs(i, j):
+  que = deque([(i, j)])
+  visited[i][j] = True
 
-  while que :
-    a, b = que.popleft()
-  
-    for i in range(4):
-      nx = a + dx[i]
-      ny = b + dy[i]
-  
-      if -1 < nx < n and -1 < ny < m and arr[nx][ny] == 1:
-        arr[nx][ny] = arr[a][b] + 1
-        que.append((nx, ny))
-      
+  while que:
+    x, y = que.popleft()
 
-bfs(0, 0)
-print(arr[-1][-1])
+    for k in range(4):
+      nx = x + dx[k]
+      ny = y + dy[k]
+      if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+        if graph[nx][ny] == 1:
+          que.append((nx, ny))
+          graph[nx][ny] = graph[x][y] + 1
+          visited[x][y] = True
+  return graph[-1][-1]
+
+print(bfs(0, 0))
