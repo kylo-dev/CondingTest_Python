fbd70bdb5a864fc010da717d3acb382c486ec586
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-02 19:22:36 +0900

    [Silver I] Title: 영역 구하기, Time: 48 ms, Memory: 34904 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,38 +1,38 @@
-from collections import deque
-m, n, k = map(int, input().split())
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
 
-arr = [[0] * n for _ in range(m)]
+M, N, K = map(int, input().split())
+graph = [[False] * M for _ in range(N)]
 
-for i in range(k):
-  x1, y1, x2, y2 = map(int, input().split())
-  for j in range(y1, y2):
-    for k in range(x1, x2):
-      arr[j][k] = 1
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def bfs(x, y):
-  que = deque([(x, y)])
-  arr[x][y] = 1
-  size = 1
-  while que:
-    x, y = que.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if 0 <= nx < m and 0 <= ny < n and arr[nx][ny] == 0:
-        arr[nx][ny] = 1
-        que.append((nx, ny))
-        size += 1
-  return size
-
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 result = []
-for i in range(m):
-  for j in range(n):
-      if arr[i][j] == 0:
-        result.append(bfs(i, j))
 
+for _ in range(K):
+  x1, y1, x2, y2 = map(int, input().split())
+  for x in range(x1, x2):
+    for y in range(y1, y2):
+      graph[x][y] = True
+
+def dfs(x, y):
+  global cnt
+  cnt += 1
+  graph[x][y] = True
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 0 <= nx < N and 0 <= ny < M and not graph[nx][ny]:
+      dfs(nx, ny)
+  return cnt
+
+for i in range(N):
+  for j in range(M):
+    if not graph[i][j]:
+      cnt = 0
+      result.append(dfs(i, j))
+
+result.sort()
 print(len(result))
-print(' '.join(map(str, sorted(result))))
+print(' '.join(map(str, result)))
