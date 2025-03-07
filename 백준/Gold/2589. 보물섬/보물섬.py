3f37e68f52ac4f4f9eb55ffa0a349c272a63845b
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-07 14:08:49 +0900

    [Gold V] Title: 보물섬, Time: 4592 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -1,38 +1,39 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+import copy
 
-N, M = map(int, input().split())
-graph = [list(input().rstrip()) for _ in range(N)]
+L, W = map(int, input().split())
+
+graph = [list(input().rstrip()) for _ in range(L)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+result = 0
+
 def bfs(i, j):
-  visited = [[0] * M for _ in range(N)]
   que = deque([(i, j)])
   visited[i][j] = 1
-  result = 0
 
   while que:
     x, y = que.popleft()
-
     for k in range(4):
       nx = x + dx[k]
       ny = y + dy[k]
-
-      if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 'L':
-        if visited[nx][ny] == 0:
-          que.append((nx, ny))
-          visited[nx][ny] = visited[x][y] + 1
-          result = max(result, visited[nx][ny])
-  return result - 1
+      if 0 <= nx < L and 0 <= ny < W and graph[nx][ny] == 'L' and not visited[nx][ny]:
+        que.append((nx, ny))
+        visited[nx][ny] = visited[x][y] + 1
+  
+  return visited
 
 result = 0
-for i in range(N):
-  for j in range(M):
-    if graph[i][j] == "L":
-      cnt = bfs(i, j)
-      result = max(result, cnt)
+for i in range(L):
+  for j in range(W):
+    if graph[i][j] == 'L':
+      visited = [[0] * W for _ in range(L)]
+      sub_res = bfs(i, j)
+      result = max(result, max(max(s) for s in sub_res))
 
-print(result)
+
+print(result - 1)
