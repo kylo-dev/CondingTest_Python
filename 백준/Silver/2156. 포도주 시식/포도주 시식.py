798f6d38ca77d349b3ae6bf0b54ac5fa5e9f9921
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-19 13:43:57 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 35000 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,17 +1,17 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-arr = [0] * 10000
-for i in range(n):
+N = int(input())
+arr = [0] * 100000
+for i in range(N):
   arr[i] = int(input())
 
-dp = [0] * 10000
-
-
+dp = [0] * 100000
 dp[0] = arr[0]
 dp[1] = arr[0] + arr[1]
 dp[2] = max(arr[0] + arr[2], arr[1] + arr[2], dp[1])
 
-for i in range(3, n):
-  dp[i] = max(dp[i-2] + arr[i], dp[i-1], arr[i] + arr[i-1] + dp[i-3])
+for i in range(3, N):
+  dp[i] = max(arr[i] + dp[i-2], arr[i] + arr[i-1] + dp[i-3], dp[i-1])
 
 print(max(dp))
