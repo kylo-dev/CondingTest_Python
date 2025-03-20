c5af8ba6e0fdecd84b25302a817d60ecdaa4058c
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-20 14:41:05 +0900

    [Gold IV] Title: 뱀, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,18 +1,20 @@
-# 보드 크기
-n = int(input())
-data = [[0] * (n+1) for _ in range(n+1)]
+import sys
+input = sys.stdin.readline
 
-# 사과 개수
-k = int(input())
-for _ in range(k):
-  a, b = map(int, input().split())
-  data[a][b] = 1
+N = int(input())
+K = int(input())
 
-l = int(input())
-info = []
-for _ in range(l):
-  x, c = input().split()
-  info.append((int(x), c))
+arr = [[0] * (N+1) for _ in range(N+1)]
+
+for _ in range(K):
+  x, y = map(int, input().split())
+  arr[x][y] = 1
+
+L = int(input())
+moves = []
+for _ in range(L):
+  time, direct = input().split()
+  moves.append((int(time), direct))
 
 # 동남서북
 dx = [0, 1, 0, -1]
@@ -23,39 +25,41 @@ def turn(direction, c):
     direction = (direction - 1) % 4
   else:
     direction = (direction + 1) % 4
-
+  
   return direction
 
 def simulate():
   x, y = 1, 1
+  arr[x][y] = 2
+
   direction = 0
-  index = 0
+  move_cnt = 0
   time = 0
+
   q = [(x, y)]
-  data[x][y] = 2
-  
+
   while True:
     nx = x + dx[direction]
     ny = y + dy[direction]
 
-    if 1 <= nx <= n and 1 <= ny <= n and data[nx][ny] != 2:
-      if data[nx][ny] == 0:
-        data[nx][ny] = 2
-        qx, qy = q.pop(0)
+    if 1 <= nx <= N and 1 <= ny <= N and arr[nx][ny] != 2:
+      if arr[nx][ny] == 0:
+        arr[nx][ny] = 2
         q.append((nx, ny))
-        data[qx][qy] = 0
-      elif data[nx][ny] == 1:
-        data[nx][ny] = 2
+        qx, qy = q.pop(0)
+        arr[qx][qy] = 0
+      elif arr[nx][ny] == 1:
+        arr[nx][ny] = 2
         q.append((nx, ny))
     else:
       time += 1
       break
-
+      
     x, y = nx, ny
     time += 1
-    if index < l and info[index][0] == time:
-      direction = turn(direction, info[index][1])
-      index += 1
+    if move_cnt < L and time == moves[move_cnt][0]:
+      direction = turn(direction, moves[move_cnt][1])
+      move_cnt += 1
   return time
 
 print(simulate())
