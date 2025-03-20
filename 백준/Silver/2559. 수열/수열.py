85179ac5b30e7de8a3e00998aec1c70c5e83ec20
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-20 15:05:53 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,12 +1,11 @@
-
 N, K = map(int, input().split())
-nums = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-max_sum = sum(nums[:K])
-win_sum = max_sum
+win_sum = sum(arr[:K])
+max_sum = win_sum
 
 for i in range(K, N):
-  win_sum = win_sum - nums[i-K] + nums[i]
+  win_sum = win_sum + arr[i] - arr[i-K]
   max_sum = max(max_sum, win_sum)
 
 print(max_sum)
