46871f12737cce1513d523eae09bfc06f8322ded
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-20 15:59:59 +0900

    [level 1] Title: 모의고사, Time: 2.41 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,21 +1,23 @@
 def solution(answers):
-    answer = []
-    arr1 = [1,2,3,4,5] # 5
-    arr2 = [2,1,2,3,2,4,2,5] # 7
-    arr3 = [3,3,1,1,2,2,4,4,5,5] # 10
     
-    score = [0,0,0]
+    student1 = [1, 2, 3, 4, 5] # 5
+    student2 = [2, 1, 2, 3, 2, 4, 2, 5] # 8
+    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # 10
     
-    for i in range(len(answers)):        
-        if answers[i] == arr1[i%len(arr1)]:
+    score = [0, 0, 0]
+    
+    for i in range(len(answers)):
+        if answers[i] == student1[i % 5]:
             score[0] += 1
-        if answers[i] == arr2[i%len(arr2)]:
+        if answers[i] == student2[i % 8]:
             score[1] += 1
-        if answers[i] == arr3[i%len(arr3)]:
+        if answers[i] == student3[i % 10]:
             score[2] += 1
     
-    for i, v in enumerate(score):
-        if max(score) == v:
+    answer = []
+    max_score = max(score)
+    for i in range(len(score)):
+        if max_score == score[i]:
             answer.append(i+1)
     
     return answer
