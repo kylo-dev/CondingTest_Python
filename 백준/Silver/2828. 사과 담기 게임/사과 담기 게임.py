0c80a1d79a6286be7c578d21c84acc3dbbc67e81
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-21 18:33:53 +0900

    [Silver V] Title: 사과 담기 게임, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
@@ -1,18 +1,16 @@
 N, M = map(int, input().split())
 J = int(input())
-
 apples = [int(input()) for _ in range(J)]
 
 cnt = 0
 now = 1
 for apple in apples:
-  if now <= apple and now + (M - 1) >= apple:
+  if now <= apple <= now + (M - 1):
     continue
   elif now > apple:
-    cnt += abs(now - apple)
+    cnt += abs(apple - now)
     now = apple
   else:
     cnt += apple - (now + M - 1)
     now = apple - (M - 1)
-
 print(cnt)
