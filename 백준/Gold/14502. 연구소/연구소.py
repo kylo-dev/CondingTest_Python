f7c1be77ee00ff34a0733c23c978a94da4676d74
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-25 22:30:39 +0900

    [Gold IV] Title: 연구소, Time: 1596 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,31 +1,28 @@
 import sys
+input = sys.stdin.readline
 from collections import deque
 from itertools import combinations
-import copy
-input = sys.stdin.readline
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
 
+arr = [list(map(int, input().split())) for _ in range(N)]
 safe = []
+virus = []
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# 안전 영역 좌표 구하기
+# 안전 영역 좌표
 for i in range(N):
   for j in range(M):
-    if graph[i][j] == 0:
+    if arr[i][j] == 0:
       safe.append((i, j))
+    elif arr[i][j] == 2:
+      virus.append((i, j))
 
-# 바이러스 감염 bfs
-def bfs(arr):
-  que = deque()
-
-  for i in range(N):
-    for j in range(M):
-      if arr[i][j] == 2:
-        que.append((i, j))
+def bfs():
+  que = deque(virus)
+  infected = []
 
   while que:
     x, y = que.popleft()
@@ -34,25 +31,25 @@ def bfs(arr):
       ny = y + dy[k]
       if 0 <= nx < N and 0 <= ny < M and arr[nx][ny] == 0:
         arr[nx][ny] = 2
+        infected.append((nx, ny))
         que.append((nx, ny))
+  
+  safe_count = sum(row.count(0) for row in arr)
 
-# 벽을 세운 후 안전 영역 카운트
-def safe_zone(arr):
-  cnt = 0
-  for i in arr:
-    for j in i:
-      if j == 0:
-        cnt += 1
-  return cnt
+  for x, y in infected:
+    arr[x][y] = 0
+  
+  return safe_count
 
-# 조합으로 3개의 벽 세우기
+
 result = 0
 for comb in combinations(safe, 3):
-  arr = copy.deepcopy(graph)
   for x, y in comb:
     arr[x][y] = 1
-  bfs(arr)
-  result = max(result, safe_zone(arr))
-    
-# 값 출력
+  
+  result = max(result, bfs())
+  
+  for x, y in comb:
+    arr[x][y] = 0
+
 print(result)
