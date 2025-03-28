5661f62623030c128814963c166c0fc9b2059f72
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-28 15:30:23 +0900

    [level 4] Title: 징검다리, Time: 148.02 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,30 +1,34 @@
 def solution(distance, rocks, n):
+    
+    # 바위 간의 거리가 최소인 상황에서 최대 값을 구해야 한다.
     left = 1
     right = distance
     
     rocks.sort()
     rocks.append(distance)
+    answer = 0
     
     while left <= right:
         mid = (left + right) // 2
-        delete_rock = 0
+        del_rock = 0
         prev_rock = 0
         
         for rock in rocks:
             dist = rock - prev_rock
+            
             if dist < mid:
-                delete_rock += 1
-                
-                if delete_rock > n:
+                del_rock += 1
+                # 허용 가능한 거리(mid)가 아니였음
+                if del_rock > n:
                     break
             else:
-                prev_rock = rock
-        
-        # 커트라인이 큰 경우
-        if delete_rock > n:
+                 prev_rock = rock   
+                    
+        if del_rock > n:
             right = mid - 1
         else:
             left = mid + 1
             answer = mid
     
     return answer
+    
