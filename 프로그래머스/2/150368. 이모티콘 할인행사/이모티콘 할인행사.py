5c6c205d8e6783aa092e720231ba5c40585b1c7e
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-30 22:01:07 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2606.02 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,17 +1,12 @@
-from collections import defaultdict
-
 def solution(users, emoticons):
     
+    N = len(emoticons)
     answer = [0, 0]
     data = [10, 20, 30, 40]
     discount = []
     
-    # 1. 서비스 가입자 늘리기
-    # 2. 판매액 늘리기
-    
-    # 이모티콘 할인율 구하기
     def dfs(temp, depth):
-        if depth == len(temp):
+        if depth == N:
             discount.append(temp[:])
             return
         
@@ -19,25 +14,26 @@ def solution(users, emoticons):
             temp[depth] += d
             dfs(temp, depth + 1)
             temp[depth] -= d
-    dfs([0] * len(emoticons), 0)
     
-    for d in range(len(discount)):
+    dfs([0] * N, 0)
+    
+    for i in range(len(discount)):
         plus_user, profit = 0, 0
         
         for user in users:
-            emoticon_buy = 0
-            for i in range(len(emoticons)):
-                if discount[d][i] >= user[0]:
-                    emoticon_buy += emoticons[i] * ((100 - discount[d][i]) / 100)
-            
-            if emoticon_buy >= user[1]:
+            total = 0
+            for j in range(N):
+                if user[0] <= discount[i][j]:
+                    total += emoticons[j] * ((100 - discount[i][j]) / 100)
+
+            if total >= user[1]:
                 plus_user += 1
             else:
-                profit += emoticon_buy
+                profit += total
         
         if answer[0] < plus_user:
             answer = [plus_user, profit]
-        elif answer[0] == plus_user:
-            if answer[1] < profit:
-                answer = [answer[0], profit]
+        elif answer[0] == plus_user and answer[1] < profit:
+            answer = [answer[0], profit]
+    
     return answer
