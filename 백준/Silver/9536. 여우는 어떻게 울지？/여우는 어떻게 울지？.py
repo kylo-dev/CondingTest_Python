c18e4257906d8336f780d6d667c499b65ac07472
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-30 22:55:30 +0900

    [Silver III] Title: 여우는 어떻게 울지?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+  all_voice = list(input().rstrip().split())
+  animal = []
+
+  while True:
+    say = input().rstrip()
+    if say == "what does the fox say?":
+      break
+
+    ani, goes, voice = say.split()
+    animal.append(voice)
+
+  all_voice = [voice for voice in all_voice if voice not in animal]
+
+  print(" ".join(all_voice))
