8281075a943e40e00862a5743cf4c6312dce7635
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-30 15:56:27 +0900

    [level 2] Title: 괄호 회전하기, Time: 310.89 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,26 +1,32 @@
-def col_str(s):
-    stk = []
-    for i in s:
-        if not stk:
-            if i == ')' or i == '}' or i == ']':
-                return False
-            stk.append(i)
-        else:
-            if i == ')' and stk[-1] == '(':
-                stk.pop()
-            elif i == '}' and stk[-1] == '{':
-                stk.pop()
-            elif i == ']' and stk[-1] == '[':
-                stk.pop()
-            else:
-                stk.append(i)
-                
-    return len(stk) == 0
+def check(gal_str):
+    
+    stack = []
+    for gal in gal_str:
+        if not len(stack):
+            stack.append(gal)
+        elif gal == "(" or gal == "[" or gal == "{":
+            stack.append(gal)
+        elif gal == ")" and stack[-1] == "(":
+            stack.pop()
+        elif gal == "]" and stack[-1] == "[":
+            stack.pop()
+        elif gal == "}" and stack[-1] == "{":
+            stack.pop()
+    
+    return True if not len(stack) else False
 
 def solution(s):
+    
+    # len(s) - 1 만큼 회정
+    # 올바른 괄호인 경우 +1
+    
+    # 올바른 괄호가 없는 경우 0 반환
+    
     answer = 0
-    for i in range(len(s)):
-        new_s = s[i:] + s[:i]
-        if col_str(new_s):
+    
+    for i in range(len(s) - 1):
+        gal_str = s[i:] + s[:i]
+        if check(gal_str):
             answer += 1
     return answer
+
