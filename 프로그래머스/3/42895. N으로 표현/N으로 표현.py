8cfb6efb9882b9e021a0d1e2579a1a26762f77e4
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-03-30 15:23:42 +0900

    [level 3] Title: N으로 표현, Time: 16.69 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -3,6 +3,8 @@ def solution(N, number):
     if N == number:
         return 1
     
+    answer = -1
+    
     arr = [set() for _ in range(8)]
     
     for i in range(8):
@@ -11,14 +13,13 @@ def solution(N, number):
     for i in range(1, 8):
         for j in range(i):
             for op1 in arr[j]:
-                for op2 in arr[i-j-1]:
+                for op2 in arr[i - j -1]:
                     arr[i].add(op1 + op2)
                     arr[i].add(op1 - op2)
                     arr[i].add(op1 * op2)
                     if op2 != 0:
                         arr[i].add(op1 // op2)
-        if number in arr[i]:
-            return i + 1
-    
-    return -1
-                    
+                        
+            if number in arr[i]:
+                return i + 1
+    return answer
