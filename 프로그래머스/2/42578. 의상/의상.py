cac0128745c0e4abb109ebd9a3ccc68738c65172
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-09 22:39:48 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,14 @@
+from collections import defaultdict
+
 def solution(clothes):
-    clo_dict = {}
     
-    for clo, ty in clothes:
-        if ty not in clo_dict:
-            clo_dict[ty] = 1
-        else:
-            clo_dict[ty] += 1
+    clothes_dict = defaultdict(list)
     
-    answer = clo_dict.values()
-    total = 1
-    for t in answer:
-        total *= (t+1)
-    return total - 1
+    for cloth_name, cloth_type in clothes:
+        clothes_dict[cloth_type].append(cloth_name)
+    
+    answer = 1
+    for cloth in clothes_dict:
+        answer *= len(clothes_dict[cloth]) + 1
+    
+    return answer - 1
