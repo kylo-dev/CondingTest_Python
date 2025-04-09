c7247ebff9464c9e156943941463d3f367cf15fb
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-09 21:15:01 +0900

    [level 2] Title: 구명보트, Time: 8.70 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,17 +2,14 @@ def solution(people, limit):
     answer = 0
     
     people.sort()
-    start, end = 0, len(people) - 1
+    left, right = 0, len(people) - 1
     
-    while start <= end:
-        total = people[start] + people[end]
-        
-        if total > limit:
-            end -= 1
+    while left <= right:
+        if people[left] + people[right] <= limit:
             answer += 1
+            left += 1
+            right -= 1
         else:
-            start += 1
-            end -= 1
+            right -= 1
             answer += 1
-    
     return answer
