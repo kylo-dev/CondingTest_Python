cd636e635b1944a1627d87779ca728d81e843aa0
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-16 21:36:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.93 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,22 +1,25 @@
-import heapq
+from heapq import heappop, heappush
+
 def solution(jobs):
-    
+
     jobs = sorted([(req, dur, idx) for idx, (req, dur) in enumerate(jobs)])
-    heap = []
-    N = len(jobs)
+    
     time, total_wait, idx = 0, 0, 0
+    N = len(jobs)
+    heap = []
     
     while idx < N or heap:
         while idx < N and jobs[idx][0] <= time:
             req, dur, job_id = jobs[idx]
-            heapq.heappush(heap, (dur, req, job_id))
+            heappush(heap, (dur, req))
             idx += 1
         
         if heap:
-            dur, req, job_id = heapq.heappop(heap)
+            dur, req = heappop(heap)
             time += dur
             total_wait += time - req
         else:
             time = jobs[idx][0]
-    return total_wait // N
     
+    return total_wait // N
+            
