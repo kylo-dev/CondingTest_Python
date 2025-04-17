4d7bfeff02fe61c66f09e54ac0a450256cd241ff
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-17 17:27:19 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 388 ms, Memory: 49660 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,34 +1,35 @@
 from collections import deque
 
-N, K = map(int, input().split())
-
-ans_time, ans_count = -1, 0
+min_time, count = -1, 0
 MAX = 100001
+
 visited = [-1] * MAX
 
+N, K = map(int, input().split())
+
 def bfs(position):
-    global ans_time, ans_count
+    global min_time, count
 
     que = deque([(position, 0)])
     visited[position] = 0
 
     while que:
-        current, time = que.popleft()
+        cur, time = que.popleft()
 
-        if current == K:
-            if ans_time == -1:
-                ans_time = time
-                ans_count = 1
-            elif time == ans_time:
-                ans_count += 1
-            continue
+        if cur == K:
+            if min_time == -1:
+                count = 1
+                min_time = time
+            elif time == min_time:
+                count += 1
         
-        for move in (current + 1, current - 1, current * 2):
-            if 0 <= move < MAX and (visited[move] == -1 or visited[move] == time + 1):
-                visited[move] = time + 1
-                que.append((move, time + 1))
+        for move in (cur + 1, cur - 1, cur * 2):
+            if 0 <= move < MAX:
+                if visited[move] == -1 or visited[move] == time + 1:
+                    que.append((move, time + 1))
+                    visited[move] = time + 1
 
 bfs(N)
 
-print(ans_time)
-print(ans_count)
+print(min_time)
+print(count)
