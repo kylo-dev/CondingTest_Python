e426041fdb7cb4f41cc6122e33842773e1a35989
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-17 16:04:40 +0900

    [Gold IV] Title: 인구 이동, Time: 4992 ms, Memory: 35148 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -2,60 +2,56 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-
 N, L, R = map(int, input().split())
 
-graph = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
+arr = [list(map(int, input().split())) for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+answer = 0
+
 def bfs(i, j, visited):
-  que = deque([(i, j)])
-  visited[i][j] = True
-  total = graph[i][j]
-  union = [(i, j)]
+    que = deque([(i, j)])
+    union = [(i, j)]
+    visited[i][j] = True
+    total = arr[i][j]
 
-  while que:
-    x, y = que.popleft()
-    
-    for k in range(4):
-      nx = x + dx[k]
-      ny = y + dy[k]
-
-      if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
-        if L <= abs(graph[x][y] - graph[nx][ny]) <= R:
-          total += graph[nx][ny]
-          visited[nx][ny] = True
-          que.append((nx, ny))
-          union.append((nx, ny))
+    while que:
+        x, y = que.popleft()
         
-  
-  if len(union) == 1:
-    return False
-  
-  new_population = total // len(union)
-  for x, y in union:
-    graph[x][y] = new_population
-  return True
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                if L <= abs(arr[x][y] - arr[nx][ny]) <= R:
+                    total += arr[nx][ny]
+                    union.append((nx, ny))
+                    que.append((nx, ny))
+                    visited[nx][ny] = True
+    
+    if len(union) == 1:
+        return False
+    
+    avg = total // len(union)
+    for x, y in union:
+        arr[x][y] = avg
+    
+    return True
 
-
-result = 0
+    
 while True:
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    moved = False
 
-  visited = [[False] * N for _ in range(N)]
-  moved = False
-
-  for i in range(N):
-    for j in range(N):
-      if not visited[i][j]:
-        if bfs(i, j, visited):
-          moved = True
-  
-  if moved:
-    result += 1
-  else:
-    break
-
-print(result)
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j] and bfs(i, j, visited):
+                moved = True
+    
+    if moved:
+        answer += 1
+    else:
+        print(answer)
+        break
+            
