a22f81f19910e6f9a488988f8460478c299ef0e7
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-22 15:45:45 +0900

    [Gold V] Title: 암호 만들기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,13 +1,13 @@
 from itertools import combinations
 
-L, C = map(int, input().split())
-letters = sorted(input().split())
-
 vowels = {'a', 'e', 'i', 'o', 'u'}
 
-for comb in combinations(letters, L):
-    volwel_count = sum(1 for c in comb if c in vowels)
-    consonant_count = L - volwel_count
+L, C = map(int, input().split())
+word = sorted(list(input().split()))
 
-    if volwel_count >= 1 and consonant_count >= 2:
+for comb in combinations(word, L):
+    vowel_count = sum(1 for c in comb if c in vowels)
+    consonant_count = L - vowel_count
+
+    if vowel_count >= 1 and consonant_count >= 2:
         print(''.join(comb))
