5b3aec3d985b5b9e19dc2cd0b76cce9d07a8716e
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-04-27 12:11:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.97 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,11 +2,12 @@ from collections import Counter
 
 def solution(participant, completion):
     
-    part = Counter(participant)
+    parti_count = Counter(participant)
+    comp_count = Counter(completion)
     
-    for name in completion:
-        part[name] -= 1
-    
-    for name, cnt in part.items():
-        if cnt > 0:
-            return name
+    for key in parti_count:
+        if key in comp_count and parti_count[key] != comp_count[key]:
+            return key
+        
+        if key not in comp_count:
+            return key
