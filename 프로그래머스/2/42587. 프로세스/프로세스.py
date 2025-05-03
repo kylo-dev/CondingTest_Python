0ae83d2ee7cf628f1a15b3b3b3b622f88ff163c4
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-05-03 23:36:07 +0900

    [level 2] Title: 프로세스, Time: 2.97 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,19 +2,17 @@ from collections import deque
 
 def solution(priorities, location):
     
-    answer = []
     que = deque([(idx, priority) for idx, priority in enumerate(priorities)])
+    answer = []
     
     while que:
+        
         idx, priority = que.popleft()
         
         if que and priority < max([priority for _, priority in que]):
             que.append((idx, priority))
         else:
             answer.append(idx)
-            # cnt += 1
-            # if idx == location:
-            #     answer = cnt
-            #     break
+    
     return answer.index(location) + 1
-        
+            
