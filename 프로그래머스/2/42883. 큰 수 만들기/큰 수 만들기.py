55cabdff809b14248db8341dfb209bbfa2f2f712
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-05-03 15:38:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 86.36 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,13 +1,15 @@
 def solution(number, k):
-    answer = []
     
-    # 모든 숫자 문자열 탐색
-    for digit in number:
-        ### k가 0보다 크고, 배열 마지막 수가 탐색하는 수보다 큰지 확인
-        while answer and k > 0 and answer[-1] < int(digit):
-            answer.pop()
+    stack = []
+    
+    for num in number:
+        
+        while stack and k > 0 and stack[-1] < num:
+            stack.pop()
             k -= 1
-        ### 아닌 경우, 배열에 숫자 더하기
-        answer.append(int(digit))
+        stack.append(num)
     
-    return "".join(map(str, answer[:len(number) - k]))
+    return ''.join(stack[:len(number) - k])
+            
+    
+    
