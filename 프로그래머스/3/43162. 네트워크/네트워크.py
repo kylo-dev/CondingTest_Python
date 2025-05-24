5ef598994725f14480b8979dc7dbc80d6f1c99f7
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-05-24 14:30:30 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -2,6 +2,7 @@ from collections import deque
 
 def solution(n, computers):
     answer = 0
+    
     visited = [False for _ in range(n)]
     
     for i in range(n):
@@ -9,11 +10,13 @@ def solution(n, computers):
             que = deque([i])
             visited[i] = True
             answer += 1
+        
         while que:
             computer = que.popleft()
-            for i in range(len(computers)):
-                if not visited[i] and computers[computer][i]:
-                    que.append(i)
-                    visited[i] = True
+            for j in range(n):
+                if not visited[j] and computers[computer][j] == 1:
+                    visited[j] = True
+                    que.append(j)
     
     return answer
+                
