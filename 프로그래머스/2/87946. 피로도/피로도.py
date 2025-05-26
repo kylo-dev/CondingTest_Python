46b98b712b0aabd432fe0f535bbf74c98984a9ee
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-05-26 17:18:52 +0900

    [level 2] Title: 피로도, Time: 41.70 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -0,0 +1,18 @@
+def solution(k, dungeons):
+    answer = 0
+    visited = [False for _ in range(len(dungeons))]
+    
+    def dfs(k, cnt):
+        nonlocal answer
+
+        if cnt > answer:
+            answer = cnt
+
+        for i in range(len(dungeons)):
+            if not visited[i] and k >= dungeons[i][0]:
+                visited[i] = True
+                dfs(k - dungeons[i][1], cnt + 1)
+                visited[i] = False
+    
+    dfs(k, 0)
+    return answer
