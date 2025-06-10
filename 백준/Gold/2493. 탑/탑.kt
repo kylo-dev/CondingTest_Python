1197e2b03f9ffd017071ecdda3f71ddd4cb49605
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-06-10 17:07:04 +0900

    [Gold V] Title: 탑, Time: 912 ms, Memory: 158744 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val N = readln().toInt()
+    val arr = readln().split(" ").map { it.toInt() }
+    val answer = IntArray(N)
+    val stack = ArrayDeque<Int>()
+
+    for (i in N - 1 downTo 0) {
+        while (stack.isNotEmpty() && arr[i] >= arr[stack.last()]) {
+            val idx = stack.removeLast()
+            answer[idx] = i + 1
+        }
+        stack.add(i)
+    }
+    
+    println(answer.joinToString(" "))
+}
