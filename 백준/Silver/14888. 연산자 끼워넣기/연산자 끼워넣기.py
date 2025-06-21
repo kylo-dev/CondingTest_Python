24c344691e0ec67376ec9c6b0d666b524e81969a
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-06-21 23:11:08 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,5 +1,5 @@
 import sys
-input = sys.stdin.readline
+from itertools import permutations
 
 N = int(input())
 arr = list(map(int, input().split()))
@@ -7,31 +7,31 @@ operations = list(map(int, input().split()))
 
 max_result = -float('inf')
 min_result = float('inf')
+
 def back(idx, total, ops):
     global max_result, min_result
+
     if idx == N:
         max_result = max(max_result, total)
         min_result = min(min_result, total)
         return
-
+    
     for i in range(4):
         if ops[i] > 0:
-            next_ops = ops[:]
-            next_ops[i] -= 1
-
+            ops[i] -= 1
             if i == 0:
-                next_total = total + arr[idx]
+                back(idx + 1, total + arr[idx], ops)
             elif i == 1:
-                next_total = total - arr[idx]
+                back(idx + 1, total - arr[idx], ops)
             elif i == 2:
-                next_total = total * arr[idx]
-            else:
+                back(idx + 1, total * arr[idx], ops)
+            elif i == 3:
                 if total < 0:
-                    next_total = -(-total // arr[idx])
+                    total = -(-total // arr[idx])
                 else:
-                    next_total = total // arr[idx]
-            
-            back(idx + 1, next_total, next_ops)
+                    total //= arr[idx]
+                back(idx + 1, total, ops)
+            ops[i] += 1
 
 back(1, arr[0], operations)
 
