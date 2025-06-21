31b510176d48a1c1e2cb566a6266da0b98a2180a
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-06-21 11:25:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,50 +1,39 @@
 import sys
 input = sys.stdin.readline
-from collections import defaultdict
 
 N = int(input())
 arr = list(map(int, input().split()))
-operations = map(int, input().split())
-
-operation_dict = {"+": 0, "-":0, "*": 0, "//": 0}
-
-for idx, oper in enumerate(operations):
-    if idx == 0 and oper != 0:
-        operation_dict["+"] += oper
-    elif idx == 1 and oper != 0:
-        operation_dict["-"] += oper
-    elif idx == 2 and oper != 0:
-        operation_dict["*"] += oper
-    elif idx == 3 and oper != 0:
-        operation_dict["//"] += oper
+operations = list(map(int, input().split()))
 
 max_result = -float('inf')
 min_result = float('inf')
-def back(idx, total):
+def back(idx, total, ops):
     global max_result, min_result
-    if idx == len(arr) - 1:
+    if idx == N:
         max_result = max(max_result, total)
         min_result = min(min_result, total)
         return
 
-    for oper, cnt in operation_dict.items():
-        if cnt > 0:
-            operation_dict[oper] -= 1
-            if oper == "+":
-                back(idx + 1, total + arr[idx+1])
-            elif oper == "-":
-                back(idx + 1, total - arr[idx+1])
-            elif oper == "*":
-                back(idx + 1, total * arr[idx+1])
-            elif oper == "//":
-                if total < 0 :
-                    total = -(-total // arr[idx+1])
-                else:
-                    total //= arr[idx+1]
-                back(idx + 1, total)
-            operation_dict[oper] += 1
+    for i in range(4):
+        if ops[i] > 0:
+            next_ops = ops[:]
+            next_ops[i] -= 1
 
-back(0, arr[0])
+            if i == 0:
+                next_total = total + arr[idx]
+            elif i == 1:
+                next_total = total - arr[idx]
+            elif i == 2:
+                next_total = total * arr[idx]
+            else:
+                if total < 0:
+                    next_total = -(-total // arr[idx])
+                else:
+                    next_total = total // arr[idx]
+            
+            back(idx + 1, next_total, next_ops)
+
+back(1, arr[0], operations)
 
 print(max_result)
 print(min_result)
