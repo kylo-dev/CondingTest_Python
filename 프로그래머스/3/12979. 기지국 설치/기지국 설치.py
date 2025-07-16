c3f5bfacb17ee171504fe43869781118352238ca
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-07-16 16:14:06 +0900

    [level 3] Title: 기지국 설치, Time: 2.09 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,20 @@
+import math
+def solution(n, stations, w):
+    answer = 0
+    prev_end = 1
+    
+    for station in stations:
+        start = station - w
+        end = station + w
+        
+        if prev_end < start:
+            diff = start - prev_end
+            answer += math.ceil(diff / (w * 2 + 1))
+        
+        prev_end = end + 1
+    
+    if prev_end <= n:
+        diff = n - prev_end + 1
+        answer += math.ceil(diff / (w * 2 + 1))
+
+    return answer
