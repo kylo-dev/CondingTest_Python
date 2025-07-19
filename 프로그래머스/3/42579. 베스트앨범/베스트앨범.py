c55462b1a92d0dd41a07ccc5ce4c6ecbaa00e2b4
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-07-19 13:27:13 +0900

    [level 3] Title: 베스트앨범, Time: 0.13 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -2,29 +2,24 @@ from collections import defaultdict
 
 def solution(genres, plays):
     answer = []
-    
+    genre_sum_dict = defaultdict(int)
     genre_dict = defaultdict(list)
+    
+    # 장르별 총 플레이 수 집계
     for i in range(len(genres)):
-        genre_dict[genres[i]].append([plays[i], i])
+        genre_sum_dict[genres[i]] += plays[i]
+        genre_dict[genres[i]].append((i, plays[i]))  # 동시에 삽입
+
+    # 총 플레이 수 기준으로 장르 정렬
+    sorted_genre_sum = sorted(genre_sum_dict.items(), key=lambda x: -x[1])
     
-    genre_rank = {}
+    # 각 장르 내에서 곡을 (재생 수 내림차순, 고유 번호 오름차순)으로 정렬
     for genre in genre_dict:
-        total = sum([i[0] for i in genre_dict[genre]])
-        genre_rank[genre] = total
+        genre_dict[genre].sort(key=lambda x: (-x[1], x[0]))
+
+    # 각 장르에서 최대 2개씩 추출
+    for genre, _ in sorted_genre_sum:
+        for i in range(min(2, len(genre_dict[genre]))):
+            answer.append(genre_dict[genre][i][0])
     
-    genre_rank = sorted(genre_rank.items(), key = lambda x: -x[1])
-    
-    for i in range(len(genre_rank)):
-        cnt = 0
-        genre = genre_rank[i][0]
-        
-        rank_list = genre_dict[genre]
-        rank_list.sort(key = lambda x : (-x[0], x[1]))
-        
-        for play, idx in genre_dict[genre]:
-            if cnt == 2:
-                break
-            else:
-                answer.append(idx)
-                cnt += 1
     return answer
