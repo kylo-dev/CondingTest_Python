21233e57f205d56014a9a2faa48dd9b39b980848
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-07-31 21:45:14 +0900

    [Silver IV] Title: 스택, Time: 372 ms, Memory: 26020 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.kt
+++ 백준/Silver/10828. 스택/스택.kt
@@ -1,17 +1,18 @@
 fun main() {
-    var N = readln().toInt()
-    var arr = mutableListOf<Int>()
+    val N: Int = readln().toInt()
 
-    repeat(N) {
-        var cmd = readln().split(" ")
+    val arr = mutableListOf<Int>()
 
-        when (cmd[0]) {
-            "push" -> arr.add(cmd[1].toString().toInt())
-            "pop" -> if (arr.isEmpty()) println(-1) else println(arr.removeLast())
-            "top" -> if (arr.isEmpty()) println(-1) else println(arr[arr.lastIndex])
+    for (i in 0 until N) {
+        val input = readln().split(" ")
+
+        when (input[0]) {
+            "push" -> arr.add(input[1].toInt())
+            "top" -> if (arr.isNotEmpty()) println(arr.last()) else println(-1)
+            "pop" -> if (arr.isNotEmpty()) println(arr.removeLast()) else println(-1)
             "size" -> println(arr.size)
             "empty" -> if (arr.isEmpty()) println(1) else println(0)
+            else -> null
         }
     }
-
 }
