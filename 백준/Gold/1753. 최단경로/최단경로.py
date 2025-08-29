3779ab2f416262e2fefbc4f620078cf20236b628
gyeom <103489352+kylo-dev@users.noreply.github.com>
2025-08-29 13:19:55 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 69300 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,32 +1,33 @@
 import sys
 import heapq
 input = sys.stdin.readline
-
 INF = 10 ** 9
 
 V, E = map(int, input().split())
-start = int(input())
+K = int(input())
 
 graph = [[] for _ in range(V + 1)]
 dist = [INF] * (V + 1)
-dist[start] = 0
-
-hq = [(0, start)]
+dist[K] = 0
 
 for _ in range(E):
   u, v, w = map(int, input().split())
   graph[u].append((v, w))
 
+hq = [(0, K)]
+
 while hq:
   d, x = heapq.heappop(hq)
+
   if d > dist[x]:
     continue
 
   for connected_x, weight in graph[x]:
-    nd = d + weight
-    if nd < dist[connected_x]:
-      dist[connected_x] = nd
-      heapq.heappush(hq, (nd, connected_x))
+    dx = d + weight
+
+    if dx < dist[connected_x]:
+      dist[connected_x] = dx
+      heapq.heappush(hq, (dx, connected_x))
 
 for i in range(1, V + 1):
   if dist[i] == INF:
